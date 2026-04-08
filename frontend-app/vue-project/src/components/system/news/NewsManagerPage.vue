c1ef97aabc07db90d699abddd5ec96952278f319
Valentins Jermakovs <jermakovsvalentins@gmail.com>
2026-04-08 12:39:11 +0300

    Small updates

--- frontend-app/vue-project/src/components/system/news/NewsManagerPage.vue
+++ frontend-app/vue-project/src/components/system/news/NewsManagerPage.vue
@@ -1,7 +1,24 @@
 <template>
-    <div class="w-full p-5 flex flex-col items-center">
+    <div class="w-full p-5 flex flex-col items-center gap-2" v-if="!newsStore.selectedNews">
         <NewsSearch></NewsSearch>
         <NewsTable></NewsTable>
+        <button class="btn btn-primary">
+            <font-awesome-icon icon="fa-solid fa-plus" />
+            Add news
+        </button>
+    </div>
+    <div v-else class="p-4 border border-base-300 bg-base-200">
+        <h1 class="text-2xl font-bold mb-3">{{ newsStore.selectedNews.title }}</h1>
+
+        <img v-if="newsStore.selectedNews.coverImage" :src="newsStore.selectedNews.coverImage"
+            class="w-full h-64 object-cover mb-3" />
+
+        <!-- Šeit renderējam HTML saturu -->
+        <div v-html="newsStore.selectedNews.content" class="text-base text-base-content/80 mb-3"></div>
+
+        <button class="btn btn-secondary" @click="newsStore.selectedNews = null">
+            Back to all news
+        </button>
     </div>
 </template>
 
@@ -9,12 +26,21 @@
 import NewsSearch from './NewsSearch.vue';
 import NewsTable from './NewsTable.vue';
 
+import { useNewsStore } from '@/stores/news';
+
 export default {
     name: 'NewsManagerPage',
     components: {
         NewsTable,
         NewsSearch
+    },
+
+    data() {
+        return {
+            newsStore: useNewsStore(),
+        }
     }
+
 }
 </script>
 
--- frontend-app/vue-project/src/components/system/news/NewsTable.vue
+++ frontend-app/vue-project/src/components/system/news/NewsTable.vue
@@ -1,62 +1,49 @@
 <template>
-    <div class="w-full border border-base-300 bg-base-200 p-4">
-        <table class="table w-full">
-            <thead>
-                <tr>
-                    <th>Creation Date</th>
-                    <th>Title</th>
-                    <th>Content</th>
-                    <th>Oparations</th>
-                </tr>
-            </thead>
+  <div class="w-full border border-base-300 bg-base-200 p-4">
+    <table class="table w-full">
+      <thead>
+        <tr>
+          <th>Creation Date</th>
+          <th>Title</th>
+          <th>Content</th>
+          <th>Operations</th>
+        </tr>
+      </thead>
 
-            <tbody>
-                <tr v-for="(row, index) in newsStore.news" :key="index">
-                    <td>
-                        <div>
-                            {{ row.createdAt.split(' ')[0] }}
-                        </div>
-                    </td>
+      <tbody>
+        <tr v-for="(row, index) in newsStore.news" :key="index">
+          <td>{{ row.createdAt.split(' ')[0] }}</td>
+          <td>{{ row.title }}</td>
+          <td>
+            <div>{{ truncateContent(row.content, 100) }}</div>
+          </td>
 
-                    <td>
-                        <div>
-                            {{ row.title }}
-                        </div>
-                    </td>
-
-                    <td>
-                        <div>
-                            {{ row.content }}
-                        </div>
-                    </td>
-
-                    <td class="flex gap-2">
-                        <button class="btn btn-sm btn-info">
-                            View
-                        </button>
-                        <button class="btn btn-sm btn-primary">
-                            Edit
-                        </button>
-                        <button class="btn btn-sm btn-error"
-                            @click="openDelete(row.id)">
-                            Delete
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+          <td class="flex gap-2">
+            <button class="btn btn-sm btn-info" @click="selectNews(row)">
+              View
+            </button>
+            <button class="btn btn-sm btn-primary">
+              Edit
+            </button>
+            <button class="btn btn-sm btn-error" @click="openDelete(row.id)">
+              Delete
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
     <!-- Remove News Modal -->
     <BaseDialog
-    title="Delete news"
-    cancel-text="cancel"
-    confirm-text="delete"
-    v-model="openDeleteModal"
-    @confirm="deleteNews"
+      title="Delete news"
+      cancel-text="cancel"
+      confirm-text="delete"
+      v-model="openDeleteModal"
+      @confirm="deleteNews"
     >
-        <p>Are you sure you want to delete this news?</p>
+      <p>Are you sure you want to delete this news?</p>
     </BaseDialog>
+  </div>
 </template>
 
 <script>
@@ -64,28 +51,41 @@ import BaseDialog from '@/components/common/BaseDialog.vue';
 import { useNewsStore } from '@/stores/news';
 
 export default {
-    name: 'NewsTable',
-    components: { BaseDialog },
-    data() {
-        return {
-            newsStore: useNewsStore(),
-            openDeleteModal: false,
-            deleteId: null
-        }
-    },
-    methods: {
-        openDelete(row) {
-            this.openDeleteModal = true,
-            this.deleteId = row
-        },
-        deleteNews() {
-            this.newsStore.deleteNews(this.deleteId)
-            this.openDeleteModal = false
-        }
-    },
-    mounted() {
-        this.newsStore.getAllNews();
+  name: 'NewsTable',
+  components: { BaseDialog },
+  data() {
+    return {
+      newsStore: useNewsStore(),
+      openDeleteModal: false,
+      deleteId: null
     }
+  },
+  methods: {
+    openDelete(row) {
+      this.openDeleteModal = true
+      this.deleteId = row
+    },
+    deleteNews() {
+      this.newsStore.deleteNews(this.deleteId)
+      this.openDeleteModal = false
+    },
+    selectNews(row) {
+      this.newsStore.selectedNews = row;
+    },
+
+    // -----------------------
+    // truncate content and remove HTML tags
+    // -----------------------
+    truncateContent(content, length = 100) {
+      const textOnly = content.replace(/<\/?[^>]+(>|$)/g, ""); // no HTML
+      if (textOnly.length <= length) return textOnly;
+      return textOnly.substring(0, length) + '...';
+    }
+  },
+  mounted() {
+    this.newsStore.getAllNews();
+    this.newsStore.selectedNews = null;
+  }
 }
 </script>
 
